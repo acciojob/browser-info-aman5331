0c6d0cfcf7a7ee41e2221b97e58e5aa615bec975
Amandeep Saxena <75635658+aman5331@users.noreply.github.com>
2023-06-22 12:58:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const browserName = navigator.appName;
+    const version = navigator.appVersion;
+
+    // Create the message
+    const message = "You are using " + browserName + " version " + version;
+
+    // Update the browser-info div with the message
+    const browserInfoElement = document.getElementById('browser-info');
+    browserInfoElement.textContent = message;
